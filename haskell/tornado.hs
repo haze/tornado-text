578555c573888ffe8fd46f472bdeff801a66d378
nv-vn <nv@cock.li>
2016-02-17 01:10:16 -0500

    Fixed too few copies (should be original + 10 (11), not 10)

--- haskell/tornado.hs
+++ haskell/tornado.hs
@@ -10,5 +10,5 @@ main = do putStr "Enter some text: "
           putStr "Enter line amount: "
           hFlush stdout
           num <- getLine
-          let tornado = take (read num) $ iterate shift text
+          let tornado = take ((read num) + 1) $ iterate shift text
           mapM_ putStrLn tornado
