830bfc9f479ad9d7e56d069ad8ce41e6b5fca73e
nv-vn <nv@cock.li>
2016-02-17 01:05:17 -0500

    Added Haskell

---
+++ haskell/tornado.hs
@@ -0,0 +1,14 @@
+import System.IO
+
+shift :: String -> String
+shift "" = ""
+shift (c:cs) = cs ++ [c]
+
+main = do putStr "Enter some text: "
+          hFlush stdout
+          text <- getLine
+          putStr "Enter line amount: "
+          hFlush stdout
+          num <- getLine
+          let tornado = take (read num) $ iterate shift text
+          mapM_ putStrLn tornado
